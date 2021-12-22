f5fd01a5d3345b9164931508e2332a9092c3c939
ethanbar11 <ethanbarbar@gmail.com>
2021-12-22 20:31:19 +0200

    ff

--- lecture_6/ascii_transformer.py
+++ lecture_6/ascii_transformer.py
@@ -1,13 +1,45 @@
-def make_letter_opposite(letter):
+# def make_letter_opposite(letter):
+#     ascii_value = ord(letter)
+#     if 65 <= ascii_value <= 90:
+#         ascii_value += 32
+#     # This means it is a small letter
+#     elif 97 <= ascii_value <= 122:
+#         ascii_value -= 32
+#     else:
+#         pass
+#     return chr(ascii_value)
+
+
+def make_lower(letter):
     ascii_value = ord(letter)
+    # This means its an upper letter
     if 65 <= ascii_value <= 90:
         ascii_value += 32
-    # This means it is a small letter
-    elif 97 <= ascii_value <= 122:
+    else:
+        pass
+    return chr(ascii_value)
+
+
+def make_upper(letter):
+    ascii_value = ord(letter)
+    # This means its an lower case letter
+    if 97 <= ascii_value <= 122:
         ascii_value -= 32
     else:
         pass
     return chr(ascii_value)
 
-print(make_letter_opposite('z'))
+
+def lower_upper(num, s):
+    new_s = ''
+    if num == 1:
+        for letter in s:
+            new_s = new_s + make_lower(letter)
+    elif num == 2:
+        for letter in s:
+            new_s = new_s + make_upper(letter)
+    return new_s
+
+print(lower_upper(2,'Hello MYAGKafaef234124;;f'))
+# print(make_letter_opposite('z'))
 # print(ord('A'))
--- lecture_6/fff.py
+++
@@ -1,8 +0,0 @@
-num=int(input('enter a number between 1 and 10,000 '))
-if 1<=num<=10000:
-    print(num)
-else:
-    try:
-        num()
-    except ValueError(num):
-        print('your number is out of range, please try again')
