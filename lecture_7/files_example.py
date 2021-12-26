50b7e5de4ab5ae24637240a8aad910cc02ab6b01
ethanbar11 <ethanbarbar@gmail.com>
2021-12-26 18:45:16 +0200

    ff

---
+++ lecture_7/files_example.py
@@ -0,0 +1,5 @@
+file = open(r"C:\Users\ethan\lecturing_code\int_24_11_21\lecture_7\to_read.txt", 'r')
+for line in file:
+    print(line)
+
+file.close()
---
+++ lecture_7/format_example.py
@@ -0,0 +1,7 @@
+x = 5
+y = 3
+z = 15
+print('The value of x:', x, '**** and the value of y:', y, 'And the value of z:', z)
+
+print('The value of x: {} **** and the value of y: {} and the value of z: {}'.format(x, y,z))
+print(f'The value of x: {x} **** and the value of y: {y} and the value of z: {z}')
