d1a95d647f808eb38d02589d259fd0b358577458
ethanbar11 <ethanbarbar@gmail.com>
2021-12-29 19:11:19 +0200

    ff

--- lecture_8/oop_example_1.py
+++ lecture_8/oop_example_1.py
@@ -7,9 +7,14 @@ class Item:
     def printInfo(self):
         print('Name : {} Price : {} code : {}'.format(self.name, self.price, self.code))
 
+    # def sum(self,num1,num2):
+    #     Sums num1,num2 and returns the value
+
 
 milk = Item('Milk', 50, 11)
 milk.printInfo()
 
 microwave=Item('Microwave',1000,12)
 microwave.printInfo()
+
+# sum_of_numbers=microwave.sum(1,2)
