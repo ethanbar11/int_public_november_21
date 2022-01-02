99fc00d344bb722c4e8cde0349a80350a95e8e56
ethanbar11 <ethanbarbar@gmail.com>
2022-01-02 19:18:07 +0200

    ff

--- lecture_8/lion_example.py
+++ lecture_8/lion_example.py
@@ -28,15 +28,27 @@ class Zoo:
         for lion in self.lions_in_zoo.values():
             print(lion.my_name)
 
+    def print_longest_lion_name(self):
+        if len(self.lions_in_zoo) == 0:
+            raise Exception('Cannot get longest name if the amount is 0!!!!')
+        longest_name_found = None
+        longest_name_length = -1
+        for lion in self.lions_in_zoo:
+            if len(lion.my_name) > longest_name_length:
+                longest_name_length = len(lion.my_name)
+                longest_name_found = lion.my_name
+        print('The longest lion name is:', longest_name_found)
+
 
 zoo12 = Zoo()
-for i in range(5):
-    name = input('Please enter lion name:')
-    zoo12.add_lion(name)
-# zoo12.add_lion('Shlomi')
-# zoo12.add_lion('Shlomi2')
-# zoo12.add_lion('Shlomi3')
-# zoo12.add_lion('Shlomi4')
-# zoo12.add_lion('Shlomi5')
+# for i in range(5):
+#     name = input('Please enter lion name:')
+#     zoo12.add_lion(name)
+zoo12.add_lion('Shlomi11111111111111111')
+zoo12.add_lion('Shlomi2')
+zoo12.add_lion('Shlomi3')
+zoo12.add_lion('Shlomi4')
+zoo12.add_lion('Shlomi332135')
 zoo12.print_lions()
 zoo12.count()
+zoo12.print_longest_lion_name()
