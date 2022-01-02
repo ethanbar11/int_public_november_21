0014d1be4ae4db89236cf0edaecfd0ed806da545
ethanbar11 <ethanbarbar@gmail.com>
2022-01-02 18:02:05 +0200

    ff

--- lecture_8/lion_example.py
+++ lecture_8/lion_example.py
@@ -14,15 +14,6 @@ class Zoo:
         new_lion = Lion(lion_name)
         self.lions_in_zoo.append(new_lion)
 
-    def make_my_lion_rawr(self):
-        self.my_lion.rawr()
 
-
-my_zoo = Zoo('Shlomi')
-my_zoo.make_my_lion_rawr()
-
-# Printing the name of the lion inside the zoo.
-print(my_zoo.my_lion.my_name)
-# x = Lion('Hola')
-# x.rawr()
-# print(x.sum(1, 2))
+zoo12 = Zoo()
+zoo12.add_lion('Shlomi')
--- lecture_8/points_example.py
+++ lecture_8/points_example.py
@@ -10,11 +10,15 @@ class Point:
     def get_distance_from_other_point(self, other_point):
         distance_in_x = (self.x - other_point.x) ** 2
         distance_in_y = (self.y - other_point.y) ** 2
+
+        # Math.sqrt = שורש
         return math.sqrt(distance_in_x + distance_in_y)
+
+    def bla(self):
+        print(self.x)
 
 
 class Rectangle:
-
     # upper_point and lower_point are of type Point
     def __init__(self, upper_point, lower_point):
         self.upper_point = upper_point
@@ -28,11 +32,26 @@ class Rectangle:
         return width * height
 
 
+class Triangle:
+    def __init__(self, point1, point2, point3):
+        self.point1 = point1
+        self.point2 = point2
+        self.point3 = point3
+
+    def get_perimeter(self):
+        distance1 = self.point1.get_distance_from_other_point(self.point2)
+        distance2 = self.point2.get_distance_from_other_point(self.point3)
+        distance3 = self.point3.get_distance_from_other_point(self.point1)
+        return distance1 + distance2 + distance3
+
 
 point1 = Point(4, 5)
 point2 = Point(2, 9)
-
+point3 = Point(10, 15)
 # distance = point1.get_distance_from_other_point(point2)
 
-rectangle = Rectangle(point1,point2)
-print(rectangle.get_area())
+# rectangle = Rectangle(point1, point2)
+# print(rectangle.get_area())
+
+triangle = Triangle(point1, point2, point3)
+print(triangle.get_perimeter())
