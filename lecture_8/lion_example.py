56fd8ad5c8704dad62ba6660f7be8ec355decdac
ethanbar11 <ethanbarbar@gmail.com>
2022-01-02 18:20:42 +0200

    ff

--- lecture_8/lion_example.py
+++ lecture_8/lion_example.py
@@ -8,11 +8,18 @@ class Lion:
 
 class Zoo:
     def __init__(self):
-        self.lions_in_zoo = []
+        self.lions_in_zoo = {}
 
     def add_lion(self, lion_name):
         new_lion = Lion(lion_name)
-        self.lions_in_zoo.append(new_lion)
+        self.lions_in_zoo[lion_name] = new_lion
+
+    def delete_lion(self, lion_name):
+        if lion_name not in self.lions_in_zoo:
+            raise Exception('There is no lion with that name in the zoo!')
+
+        # The syntax of deleting item in dictionary by the key.
+        del self.lions_in_zoo[lion_name]
 
 
 zoo12 = Zoo()
