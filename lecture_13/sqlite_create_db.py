de908672b3d0577d0cc3c0a7103a4b75cb24a17b
ethanbar11 <ethanbarbar@gmail.com>
2022-01-16 20:29:39 +0200

    ff

---
+++ lecture_13/sqlite_create_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+db_path=r"C:\users\ethan\pythonsqlite.db"
+create_connection(db_path)
